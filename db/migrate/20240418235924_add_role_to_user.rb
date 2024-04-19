afb2bd11d3aa8afcb23023b241716b4d9d322e86
TylerSangster <TylerSangster@gmail.com>
2024-04-18 22:53:39 -0300

    fixing issues

--- db/migrate/20240418235924_add_role_to_user.rb
+++ db/migrate/20240418235924_add_role_to_user.rb
@@ -1,5 +1,5 @@
 class AddRoleToUser < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :role, :string, null: false
+    add_column :users, :role, :string
   end
 end
--- db/migrate/20240419014459_add_role_null_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddRoleNullToUser < ActiveRecord::Migration[7.1]
-  def change
-
-    change_column :users, :role, :string, null: true
-  end
-end
