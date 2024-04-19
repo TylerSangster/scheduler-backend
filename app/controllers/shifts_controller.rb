68ef0fb909f679c9a058a3978b9c6223674612cf
TylerSangster <TylerSangster@gmail.com>
2024-04-18 23:22:57 -0300

    fixing bug

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,5 +1,5 @@
 class ShiftsController < ApplicationController
-  before_action :set_shift, only: [:show, update, destroy]
+  before_action :set_shift, only: [:show, :update, :destroy]
 
   # GET /shifts
   def index
